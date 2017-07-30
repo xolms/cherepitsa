ecab85ac96b4828d849b620afb361b40b1fbc7e4
xolms <xolms111@ya.ru>
2017-07-30 17:47:08 +0300

    first
    Signed-off-by: xolms <xolms111@ya.ru>

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {   $pass = str_random(10);
+        DB::table('users')->insert(
+            [
+                'name' => 'Кузовов Степан',
+                'email' => 'xolms111@ya.ru',
+                'password' => bcrypt($pass)
+            ]
+        );
+        echo $pass;
+        DB::table('users')->insert(
+            [
+                'name' => 'Алена Брежнева',
+                'email' => 'trishforever@mail.ru',
+                'password' => bcrypt('Fktyrf2040670')
+            ]
+        );
+
+    }
+}
