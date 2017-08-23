6d96f63a97af817736b8de943e505122e12847f0
xolms <xolms111@ya.ru>
2017-08-23 13:31:04 +0300

    123

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '37.140.192.7'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'u0379255_cherepitsa'),
+            'username' => env('DB_USERNAME', 'u0379255_sterka'),
+            'password' => env('DB_PASSWORD', '657216as'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
